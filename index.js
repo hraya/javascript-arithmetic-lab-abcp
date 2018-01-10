3b23fe2e98933bc9adb0342a9680ef51efa8968c
Jr <hraya@lssu.edu>
2018-01-10 03:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function imNotSure() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b 
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
 }
 
